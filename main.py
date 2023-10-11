21f5f2a91cea787ae9818ed34e84eb5909601f18
Marek Wenta <marek.wenta2009@gmail.com>
2023-10-11 14:38:32 +0200

    Pętle

---
+++ main.py
@@ -0,0 +1,26 @@
+'''n = int(input('podaj liczbe powtorzen'))
+
+for i in range(10): #range(0, n) - bez n
+    print(i)'''
+
+'''lista_liczb = []
+q= int(input('ile liczb chcesz podać'))
+
+for i in range('n'):
+    x = int(input('Podaj kolejną liczbę'))
+    lista_liczb.append(x)
+    print(lista_liczb)
+    
+print(lista_liczb)'''
+
+'''lista_liczb2 = [5, 10, 15, 20, 78]
+    
+for i in lista_liczb2:
+    print(i + 2)'''
+
+'''for i in range(3, 101, 3):
+    print(i)'''
+
+'''lista_liczb3 = [1, 2, 3, 4, 5, 7, 8, 9]
+for i in range(0, len(lista_liczb3), 2):
+    print(lista_liczb3[0:len(lista_liczb3):2])'''
